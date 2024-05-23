1b6ccf5a2423c20b31bd1ff6dcb1e4c3377721eb
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2024-05-23 15:58:20 +0200

    deleted not used classes from project initialization

--- sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/SampleHomeController.java
+++
@@ -1,7 +0,0 @@
-package com.ftn.sbnz.service;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class SampleHomeController {
-}
--- sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/controllers/ListingController.java
+++ sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/controllers/ListingController.java
@@ -1,12 +1,9 @@
 package com.ftn.sbnz.service.controllers;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ftn.sbnz.service.dtos.AddDiscountDTO;
