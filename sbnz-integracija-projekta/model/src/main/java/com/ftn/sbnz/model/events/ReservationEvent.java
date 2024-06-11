d56f5238d990524fdfdc9604a1b14363d8dfed85
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2024-06-11 22:22:17 +0200

    added information about irresponsible traveler when making a reservation

--- sbnz-integracija-projekta/model/src/main/java/com/ftn/sbnz/model/events/ReservationEvent.java
+++ sbnz-integracija-projekta/model/src/main/java/com/ftn/sbnz/model/events/ReservationEvent.java
@@ -10,12 +10,35 @@ import org.kie.api.definition.type.Role;
 public class ReservationEvent implements Serializable{
     private static final long serialVersionUID = 1L;
     private Long bookingId;
+    private String emailTo;
+    private String emailMessage;
 
     public ReservationEvent(Long bookingId) {
         super();
         this.bookingId = bookingId;
     }
 
+    public ReservationEvent(Long bookingId, String emailTo, String message) {
+        this.bookingId = bookingId;
+        this.emailTo = emailTo;
+        this.emailMessage = message;
+    }
+
+    public String getEmailTo() {
+        return emailTo;
+    }
+
+    public void setEmailTo(String emailTo) {
+        this.emailTo = emailTo;
+    }
+
+    public String getEmailMessage() {
+        return emailMessage;
+    }
+
+    public void setEmailMessage(String emailMessage) {
+        this.emailMessage = emailMessage;
+    }
     public Long getBookingId() {
         return bookingId;
     }
